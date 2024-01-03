f1f2b6b2ab1585ace47283eb14f5efa52977156c
sidxh <totallysiddhant@gmail.com>
2024-01-04 00:57:28 +0530

    setup the implementation and preview structure

---
+++ src/implementation/ChatboxImplementation.tsx
@@ -0,0 +1,13 @@
+// components/ChatboxImplementation.js
+import React from 'react';
+
+const ChatboxImplementation = () => {
+  return (
+    <div>
+      <h2>Chatbox Implementation</h2>
+      {/* Your Chatbox component implementation details go here */}
+    </div>
+  );
+};
+
+export default ChatboxImplementation;
---
+++ src/implementation/DropdownImplementation.tsx
@@ -0,0 +1,13 @@
+// components/DropdownImplementation.js
+import React from 'react';
+
+const DropdownImplementation = () => {
+  return (
+    <div>
+      <h2>Dropdown Implementation</h2>
+      {/* Your Dropdown component implementation details go here */}
+    </div>
+  );
+};
+
+export default DropdownImplementation;
